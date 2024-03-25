aee8dbcff4a388995db1d5497283174ccf948e11
Jordana Accrombessy <accrombessyjordana@gmail.com>
2024-03-25 09:41:30 +0100

    Ajout des fichiers de Méjo

---
+++ app/components/header.jsx
@@ -0,0 +1,74 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import { IoIosCart, IoIosPerson, IoMdMenu, IoMdClose, IoMdSearch } from "react-icons/io";
+import {
+    useState,
+    useEffect,
+    useRef,
+    useContext,
+    useCallback,
+    useReducer,
+    useMemo,
+  } from "react";
+
+export default function Header() {
+
+    const [menuState, setMenuState] = useState("menu");
+  
+
+  function onToggleMenu() {
+    setMenuState((prevState) => (prevState === "menu" ? "close" : "menu"));
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("top-[20%]");
+  }
+
+  return (
+    <header className="px-12 py-2 flex flex-wrap justify-between items-center w-[100%] mx-auto shadow-[2px_2px_5px_-1px_rgba(0,0,0,0.20),-2px_-2px_5px_-1px_rgba(0,0,0,0.05)]">
+      
+      <p className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#CC0000] to-[#181842]"><img
+              src="méjo-logo.png"
+              className="block w-[50px] h-[50px]"
+              alt="Méjo"
+            /></p>
+      
+      <div className="flex items-center gap-6">
+      <div className="nav-links bg-white duration-500 md:static absolute z-40 md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 py-5 md:py-0 md:px-1">
+        <ul className="flex md:flex-row flex-col md:items-center md:gap-[2vw] lg:gap-[1vw] gap-4">
+          <li className="inline-block p-2 lg:p-5 font-semibold  hover:border-b-2 hover:border-[#0099FF] active:text-blue-600 hover:duration-500">
+            <Link href="/">Accueil</Link>
+          </li>
+          <li className="inline-block p-2 lg:p-5 font-semibold hover:border-b-2 hover:border-[#0099FF] active:text-blue-600 hover:duration-500">
+            <Link href="/#méjo">Méjo</Link>
+          </li>
+          <li className="inline-block p-2 lg:p-5 font-semibold hover:border-b-2 hover:border-[#0099FF] active:text-blue-600 hover:duration-500">
+            <Link href="/#mission">Notre Mission</Link>
+          </li>
+          <li className="inline-block p-2 lg:p-5 font-semibold hover:border-b-2 hover:border-[#0099FF] active:text-blue-600 hover:duration-500">
+            <Link href="/#comment-ça-marche">Comment ça marche</Link>
+          </li>
+          <li className="inline-block text-center text-white p-4 rounded-lg bg-[#0099FF] font-semibold hover:duration-500">
+            <Link href="/#contact">Contactez-nous</Link>
+          </li>     
+        </ul>
+      </div>
+
+        {menuState === "menu" ? (
+          <IoMdMenu
+            onClick={() => onToggleMenu()}
+            size={20}
+            color="#0099FF"
+            className="text-3xl cursor-pointer md:hidden"
+          />
+        ) : (
+          <IoMdClose
+            onClick={() => onToggleMenu()}
+            size={20}
+            color="#0099FF"
+            className="text-3xl cursor-pointer md:hidden"
+          />
+        )}
+      </div>
+    </header>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+
+        <section className={`h-auto md:h-[400px] flex flex-wrap items-center p-8 md:p-16`}>
+            <div className={`w-full md:w-1/2 h-auto pb-4 md:pb-0 md:pr-4`}>
+                <h1 className={`text-2xl md:text-4xl font-bold py-4`}>
+                    Ne manquez plus vos prises de médicaments !
+                </h1>
+                <p className={`py-4 text-lg`}>Méjo, l&apos;application mobile qui vous rappelle de prendre vos médicaments à temps, où que vous soyez.</p>
+                <p className={`py-4 flex flex-row gap-4`}>
+                      <a href="/#contact" className={`inline-block px-4 text-lg text-white font-semibold rounded-lg py-4 bg-[#0099FF] hover:bg-white hover:text-[#0099FF]`}>Contactez-nous</a>
+                </p>
+            </div>
+            <div className={`w-full relative md:w-1/2 rounded-2xl overflow-hidden flex flex-wrap justify-center h-auto md:h-full`}>    
+                    <Image className={`w-full h-full object-cover`} src={'/mejouse.png'} width="300" height="300" alt="portfolio"  /> 
+            </div>
+        </section>
+
+        <section className={`h-auto bg-white rounded-lg my-7 relative z-10 px-8 py-4 w-[200px] md:w-[800px] flex flex-wrap justify-center mx-auto items-center shadow-md`}>
+             <div className={`w-full flex flex-col md:flex-row`}>
+                   <div className={`w-full md:w-1/3 h-auto flex flex-wrap justify-center`}>  
+                          <div className='w-[60px] h-[50px]'>
+                            <Image className={`w-full h-full object-cover`} src={'/main.png'} width="50" height="50" alt="Valeur"  /> 
+                          </div>
+                          <p className="py-3 text-center">Gérez vos prise de médicaments en toute confiance</p>              
+                   </div>
+                   <div className={`w-full md:w-1/3 h-auto flex flex-wrap justify-center`}>
+                          <div className='w-[50px] h-[50px]'>
+                            <Image className={`w-full h-full object-cover`} src={'/manfusion.png'} width="50" height="50" alt="Valeur"  /> 
+                          </div>
+                          <p className="py-3 text-center">Suivez votre évolution et développez des habitudes saines</p>
+                   </div>
+                   <div className={`w-full md:w-1/3 h-auto flex flex-wrap justify-center`}>
+                         <div className="">
+                          <div className="w-full flex flex-wrap justify-center">
+                            <div className='w-[50px] h-[50px]'>
+                              <Image className={`w-full h-full object-cover`} src={'/donnee.png'} width="50" height="50" alt="Valeur"  /> 
+                            </div>
+                          </div>
+                          <p className="py-3 text-center">Ayez un accès centralisé <br /> à vos informations <br /> médicales</p>
+                         </div>
+                   </div>
+             </div>
+        </section>
+
+        <section id='méjo' className={`h-auto text-white mt-[-60px] md:mt-[-120px] bg-[#0099FF] md:h-[400px] flex flex-wrap items-center px-8 py-16 md:p-16 lg:p-32`}>
+             <h2 className='py-4 text-center text-lg md:text-2xl font-bold'>Qu&apos;est-ce que Méjo ?</h2>
+             <p className="py-4 ">Mejo est une application mobile construite pour aider les patients à gérer  efficacement leur prise de médicaments.Elle  garantit une prise de médicaments régulière, en maintenant la conformité au traitement prescrit par les médecins. Méjo contribue à l’amélioration des  résultats de santé et à la réduction des risques de complications ou de rechutes.
+Méjo offre des rappels personnalisés pour chaque médicament.</p>
+        </section>
+
+        <section id='mission' className={`h-auto bg-[#CCEBFF] flex flex-wrap items-center p-8 md:p-16`}>
+            <div className={`pb-4 md:pb-0 w-full relative md:w-1/3 flex flex-wrap justify-center h-auto md:h-full`}>    
+                  <Image className={`w-full h-full rounded-2xl object-cover`} src={'/mission.png'} width="300" height="300" alt="portfolio"  /> 
+            </div>
+            <div className={`w-full md:w-2/3 h-auto md:pl-8`}>
+                <h2 className={`text-lg md:text-2xl font-bold py-4`}>
+                    Notre mission
+                </h2>
+                <p className={`py-4 text-lg`}>La mission de Méjo consiste à simplifier et à optimiser la gestion des traitements médicaux pour les utilisateurs en fournissant une solution aussi pratique et qu&apos;efficace pour la prise des médicaments. Méjo vise à aider les individus à suivre rigoureusement leur traitement, même en étant occupés par leurs activités quotidiennes. Avec ses fonctionnalités de rappels personnalisés, Méjo contribue à l&apos;amélioration de l&apos;adhérence au traitement et favorise de meilleurs résultats de santé pour ses utilisateurs.</p>
+                <p className={`py-4 flex flex-row gap-4`}>
+                      <a href="/#ensavoirplus" className={`inline-block px-4 text-lg text-[#0099FF] font-semibold rounded-lg py-4 bg-white hover:bg-[#0099FF] hover:text-white`}>En savoir plus</a>
+                </p>
+            </div>
+        </section>
+      
+    </main>
+  )
+}
