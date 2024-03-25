102fa8ed4e562d3aa8782eb9a06c5191f7b70c90
Jordana Accrombessy <accrombessyjordana@gmail.com>
2024-03-25 10:35:34 +0100

    Ajout d'animation aux sections

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,21 @@
+'use client'
 import Image from 'next/image'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <main className="">
 
         <section className={`h-auto md:h-[400px] flex flex-wrap items-center p-8 md:p-16`}>
-            <div className={`w-full md:w-1/2 h-auto pb-4 md:pb-0 md:pr-4`}>
+            <div data-aos='fade-up' data-aos-duration="3000" className={`w-full md:w-1/2 h-auto pb-4 md:pb-0 md:pr-4`}>
                 <h1 className={`text-2xl md:text-4xl font-bold py-4`}>
                     Ne manquez plus vos prises de médicaments !
                 </h1>
@@ -14,12 +24,12 @@ export default function Home() {
                       <a href="/#contact" className={`inline-block px-4 text-lg text-white font-semibold rounded-lg py-4 bg-[#0099FF] hover:bg-white hover:text-[#0099FF]`}>Contactez-nous</a>
                 </p>
             </div>
-            <div className={`w-full relative md:w-1/2 rounded-2xl overflow-hidden flex flex-wrap justify-center h-auto md:h-full`}>    
+            <div data-aos='fade-up' data-aos-duration="3000" className={`w-full relative md:w-1/2 rounded-2xl overflow-hidden flex flex-wrap justify-center h-auto md:h-full`}>    
                     <Image className={`w-full h-full object-cover`} src={'/mejouse.png'} width="300" height="300" alt="portfolio"  /> 
             </div>
         </section>
 
-        <section className={`h-auto bg-white rounded-lg my-7 relative z-10 px-8 py-4 w-[200px] md:w-[800px] flex flex-wrap justify-center mx-auto items-center shadow-md`}>
+        <section className={`h-auto bg-white rounded-lg my-7 relative z-10 px-8 py-4 w-full md:w-[800px] flex flex-wrap justify-center mx-auto items-center shadow-md`}>
              <div className={`w-full flex flex-col md:flex-row`}>
                    <div className={`w-full md:w-1/3 h-auto flex flex-wrap justify-center`}>  
                           <div className='w-[60px] h-[50px]'>
@@ -47,16 +57,16 @@ export default function Home() {
         </section>
 
         <section id='méjo' className={`h-auto text-white mt-[-60px] md:mt-[-120px] bg-[#0099FF] md:h-[400px] flex flex-wrap items-center px-8 py-16 md:p-16 lg:p-32`}>
-             <h2 className='py-4 text-center text-lg md:text-2xl font-bold'>Qu&apos;est-ce que Méjo ?</h2>
-             <p className="py-4 ">Mejo est une application mobile construite pour aider les patients à gérer  efficacement leur prise de médicaments.Elle  garantit une prise de médicaments régulière, en maintenant la conformité au traitement prescrit par les médecins. Méjo contribue à l’amélioration des  résultats de santé et à la réduction des risques de complications ou de rechutes.
+             <h2 data-aos='fade-down' data-aos-duration="3000" className='py-4 text-center text-lg md:text-2xl font-bold'>Qu&apos;est-ce que Méjo ?</h2>
+             <p data-aos='fade-down' data-aos-duration="3000" className="py-4 ">Mejo est une application mobile construite pour aider les patients à gérer  efficacement leur prise de médicaments.Elle  garantit une prise de médicaments régulière, en maintenant la conformité au traitement prescrit par les médecins. Méjo contribue à l’amélioration des  résultats de santé et à la réduction des risques de complications ou de rechutes.
 Méjo offre des rappels personnalisés pour chaque médicament.</p>
         </section>
 
         <section id='mission' className={`h-auto bg-[#CCEBFF] flex flex-wrap items-center p-8 md:p-16`}>
-            <div className={`pb-4 md:pb-0 w-full relative md:w-1/3 flex flex-wrap justify-center h-auto md:h-full`}>    
+            <div data-aos='fade-right' data-aos-duration="3000" className={`pb-4 md:pb-0 w-full relative md:w-1/3 flex flex-wrap justify-center h-auto md:h-full`}>    
                   <Image className={`w-full h-full rounded-2xl object-cover`} src={'/mission.png'} width="300" height="300" alt="portfolio"  /> 
             </div>
-            <div className={`w-full md:w-2/3 h-auto md:pl-8`}>
+            <div data-aos='fade-right' data-aos-duration="3000" className={`w-full md:w-2/3 h-auto md:pl-8`}>
                 <h2 className={`text-lg md:text-2xl font-bold py-4`}>
                     Notre mission
                 </h2>
@@ -66,6 +76,8 @@ Méjo offre des rappels personnalisés pour chaque médicament.</p>
                 </p>
             </div>
         </section>
+
+        
       
     </main>
   )
